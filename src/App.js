290fd147eb02d897f3cb7ce80ab81d276823be4e
Márcio <emaildomso@gmail.com>
2018-07-27 18:53:39 -0300

    feat: Exibição das prateleiras no template final
    Os livros agora são exibidos usando o template fornecido, portanto já podemos ver a página inicial com o layout final.
    
    No código foram criados novos componentes para uma melhor composição.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,96 @@
 import React, {Component} from 'react'
-// TODO: Apenas para orientar a criação da interface. Deve ser removido até o fim do projeto.
-import Template from './Template'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-class ListShelf extends React.Component {
+// TODO: Implementar mudança de estado de um livro (mudar de prateleira)
+// TODO: Implementar busca e inclusão
+// TODO: Implementar ROUTE
+
+class ListShelf extends Component {
 	render() {
 		const { books, shelf } = this.props
-//		books.map(b => {console.log(b.title)})
-//		const b = books[0]
-//		console.log(books[0] && books[0].title)
 		
 		let showingBooks = books.filter(book => book.shelf === shelf.shelf)
 		
 		return(
-			<div>
-			<p>{shelf.name}</p>
-			<ol>
-			  {showingBooks.map( book => (
-				  <li>
-					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-					<div>{book.title}</div>
-					<div>{book.authors[0]}</div>
-					<div>{book.shelf}</div>
+			<div className="bookshelf">
+			  <h2 className="bookshelf-title">{shelf.name}</h2>
+			  <div className="bookshelf-books">
+				<ol className="books-grid">
+				{showingBooks.map( book => (
+				  <li key={book.id}>
+					<div className="book">
+					  <div className="book-top">
+						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+						<div className="book-shelf-changer">
+						  <select>
+							<option value="move" disabled>Move to...</option>
+							<option value="currentlyReading">Currently Reading</option>
+							<option value="wantToRead">Want to Read</option>
+							<option value="read">Read</option>
+							<option value="none">None</option>
+						  </select>
+						</div>
+					  </div>
+					  <div className="book-title">{book.title}</div>
+					  <div className="book-authors">{book.authors[0]}</div>
+					</div>
 				  </li>
-			  ))}
-			</ol>
+				))}
+				</ol>
+			  </div>
 			</div>
 		)
 	}
 }
 
-class BooksApp extends React.Component {
+class SearchBook extends Component {
+	render(){
+		return(
+		  <div className="search-books">
+			<div className="search-books-bar">
+			  <a className="close-search" onClick={() => this.props.closeSearch()}>Close</a>
+			  <div className="search-books-input-wrapper">
+				{/*
+				  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+				  You can find these search terms here:
+				  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+				  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+				  you don't find a specific author or title. Every search is limited by search terms.
+				*/}
+				<input type="text" placeholder="Search by title or author"/>
+
+			  </div>
+			</div>
+			<div className="search-books-results">
+			  <ol className="books-grid"></ol>
+			</div>
+		  </div>
+		)
+	}
+}
+
+class BooksApp extends Component {
 	state = {
-		books: []
+		books: [], 
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+		showSearchPage: false
 	}
 	
 	componentDidMount() {
 		BooksAPI.getAll().then( books => {
 			this.setState({ books })
 		})
+	}
+	
+	closeSearch = () => {
+		this.setState({ showSearchPage: false })
 	}
 	
 	render(){
@@ -59,11 +110,26 @@ class BooksApp extends React.Component {
 		]
 		
 		return(
-		  <div>
-		  {shelves.map(shelf => (
-			<ListShelf books={this.state.books} shelf={shelf}/>
-		  ))}
-		    <Template/>
+		  <div className="app">
+			{this.state.showSearchPage ? (
+			  <SearchBook closeSearch={this.closeSearch}/>
+			) : (
+			  <div className="list-books">
+				<div className="list-books-title">
+				  <h1>MyReads</h1>
+				</div>
+	            <div className="list-books-content">
+				  {shelves.map(shelf => (
+					<div key={shelf.shelf}>
+						<ListShelf books={this.state.books} shelf={shelf}/>
+					</div>
+				  ))}
+				</div>
+				<div className="open-search">
+				  <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+				</div>
+			  </div>
+			)}
 		  </div>
 		)
 	}
